1e6fa0bccbdf893791e1eb55f2e0242f2c76dabf
Ramkilli <rkilli@scu.edu>
2023-10-03 13:25:07 -0700

    commit

---
+++ CovertStringToUpperCase.java
@@ -0,0 +1,12 @@
+public class CovertStringToUpperCase {
+    public static String toUpperCase(String s1)
+    {
+
+        return s1.toUpperCase();
+    }
+    public static void main(String[] args) {
+        String String1 = "Read";
+        //String s2  = toUpperCase(String1);
+        System.out.println(toUpperCase(String1));
+    }
+}
