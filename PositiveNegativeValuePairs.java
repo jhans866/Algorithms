0b756163149caae21f57a97e729ba202d4935cde
Ramkilli <rkilli@scu.edu>
2024-08-31 23:44:56 -0700

    commit

---
+++ PositiveNegativeValuePairs.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class PositiveNegativeValuePairs {
+    public static void main(String[] args) {
+        int[] arr = {1,3,2,3,6,-1};
+        //HashMap<>
+        for (int i = 0; i<arr.length;i++)
+        {
+
+        }
+    }
+}
